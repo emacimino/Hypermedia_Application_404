123df0ab308f44793f5d7d840b038d9c9112e6a0
emanuele <emanuele.cimino@mail.polimi.it>
2025-05-01 11:21:23 +0200

    Page skeleton

---
+++ Pages/activityPage/activity_[id].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>activity id: {{$route.params.id}}</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Pages/teachers/teacher_[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>event id: {{$route.params.id}}</div>
+</template>
+
+<style scoped>
+
+</style>
