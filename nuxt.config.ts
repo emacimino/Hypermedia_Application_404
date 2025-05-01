a65385198afeba942e5ecaf41568a2f4465a95d2
emanuele <emanuele.cimino@mail.polimi.it>
2025-05-01 15:50:28 +0200

    adding nuxt icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,14 @@ import {resolve} from "path";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   alias:{
     "@":resolve(__dirname,"/"),
   },
+
   css: [
       "~/assets/main.css",
-  ]
+  ],
+
+  modules: ["@nuxt/icon"]
 })
