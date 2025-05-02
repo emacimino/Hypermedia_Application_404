5698fc2e0744a2a39ac0636d80f36d7eaf38471a
Gabriele Lorenzetti <127121094+GabrieleLorentz@users.noreply.github.com>
2025-05-02 11:54:51 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,40 @@
 <template>
-  <div :class = "$style.barraFooter">
-    <div :class = "$style.socialIcon">
-      <Icon name="mdi:facebook" size="45" />
-      <Icon name="mdi:instagram" size="45" />
-      <Icon name="mdi:youtube" size="45" />
+  <div :class="$style.barraFooter">
+    <div :class="$style.whiteLotusCenter">White Lotus Center</div>
+    <div :class="$style.socialIcon">
+      <Icon name="mdi:facebook" size="32" />
+      <Icon name="mdi:instagram" size="32" />
+      <Icon name="mdi:youtube" size="32" />
     </div>
-
   </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style module>
 .barraFooter {
   width: 100%;
-  height: 100px;
-  position: relative;
+  height: 140px;
   background-color: #d0f0fd;
   display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding-left: 80px;
+  box-sizing: border-box;
 }
+
+.whiteLotusCenter {
+  font-size: 24px;
+  line-height: 150%;
+  color: #1f3a5f;
+  margin-bottom: 12px;
+}
+
 .socialIcon {
+  display: flex;
+  gap: 16px;
   color: #1f3a5f;
 }
 </style>
