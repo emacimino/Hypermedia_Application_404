342e78307d3ba358e68e020ed24a208669740f0d
GiorgioSidari <giorgio1.sidari@mail.polimi.it>
2025-05-26 11:43:42 +0200

    activity pages switch languages by using description in supabase. I problem to fix: when you try to switch language directly inside the page, it does not change it, while if you change language before clicking into the activity it redirects to the right one.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import {resolve} from "path";
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -17,11 +19,11 @@ export default defineNuxtConfig({
     redirect: false,
   },
 
-  modules: ["@nuxt/icon",'@nuxtjs/supabase'],
+  modules: ["@nuxt/icon",'@nuxtjs/supabase','@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY
     }
-  }
+  },
 })
