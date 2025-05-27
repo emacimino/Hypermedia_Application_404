fc1523eef29ab0a7a9c20a44313ae593338f6c4f
Gabriele Lorenzetti <127121094+GabrieleLorentz@users.noreply.github.com>
2025-05-27 12:12:58 +0200

    pacchetti prezzi + fix generali

--- pages/aboutUs.vue
+++ pages/aboutUs.vue
@@ -1,27 +1,16 @@
 <template>
-  <div :class="$style.aboutUs">
-    <Presentation
-        :title="t('pages.aboutUs.title')"
-        :paragraphs="t('pages.aboutUs.description')"
-        image="/image 19.png"
-        :reverse="true"
-    />
-  </div>
+  <Presentation
+      :title="t('pages.aboutUs.title')"
+      :paragraphs="t('pages.aboutUs.description')"
+      image="/image 19.png"
+      :reverse="true"
+  />
 </template>
 
 <script setup lang="ts">
-import { useLanguage } from '../composables/useLanguage'
+import { useLanguage } from '~/composables/useLanguage'
 const { t } = useLanguage()
 </script>
 
 <style module>
-.aboutUs {
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-  text-align: left;
-  font-size: 64px;
-  color: #1f3a5f;
-  font-family: Inter;
-}
 </style>
