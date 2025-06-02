2deb94a590741d28e44818ad1ea09b874f455dde
GiorgioSidari <giorgio1.sidari@mail.polimi.it>
2025-06-02 10:10:12 +0200

    contacts translation in database

--- locales/en.ts
+++ locales/en.ts
@@ -4,14 +4,5 @@ export default {
     highlights: 'highlights',
     aboutUs: 'About us',
     contacts: 'Contacts',
-    pages: {
-        contacts: {
-            locationTitle: 'Where are we?',
-            address: `📍 White Lotus Center<br />via Privata Siracusa 1<br />Milano, MI 20134<br />Italy`,
-            contactTitle: 'Contacts',
-            contactDetails: `📞 Phone: +1 (310) 555-8123<br />📧 Email: info@whitelotus.com`,
-            hoursTitle: 'Opening Hours',
-            hoursDetails: `Monday – Friday: 7:00 AM – 9:00 PM<br />Saturday – Sunday: 8:00 AM – 6:00 PM`
-        }
-    }
+
 }
