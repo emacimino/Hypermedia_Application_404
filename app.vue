07668e126d4ae452a331ebc29bfd833a62a1398d
emanuele <emanuele.cimino@mail.polimi.it>
2025-06-09 17:13:15 +0200

    accessibility fix

--- app.vue
+++ app.vue
@@ -17,6 +17,16 @@
 <script setup lang="ts">
 import AppBar from "~/components/appBar.vue";
 import Footer from "~/components/Footer.vue";
+import { useLanguage } from '~/composables/useLanguage'
+import { useHead } from '#imports'
+
+const { currentLang } = useLanguage()
+
+useHead(() => ({
+  htmlAttrs: {
+    lang: currentLang.value
+  }
+}))
 </script>
 
 <style scoped>
