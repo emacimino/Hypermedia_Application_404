9e40ce92dd6184fd993f36993992f3bef9734fab
emanuele <emanuele.cimino@mail.polimi.it>
2025-06-16 11:41:20 +0200

    fix WeeklyView.vue style

--- components/Calendar/WeeklyView.vue
+++ components/Calendar/WeeklyView.vue
@@ -191,7 +191,7 @@ const wrapperClass = computed(() =>
         <span class="text-gray-600">{{ event.Time }}</span>
       </div>
 
-      <div class="mt-4 flex flex-col md:flex-row md:items-center md:gap-4">
+      <div class="flex flex-col gap-2 md:flex-row md:items-center md:gap-4">
         <nuxt-link v-if="!isActivity"
             :to="`/activityPage/${event.Course_Id}`"
             class="px-4 py-2 text-white bg-blue-300 rounded hover:bg-blue-400 hover:scale-105 active:bg-blue-400 active:scale-105"
