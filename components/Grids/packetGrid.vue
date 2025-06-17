f123f39073185bedcfad968df9cfd18c4ad6ea4f
Gabriele Lorenzetti <127121094+GabrieleLorentz@users.noreply.github.com>
2025-06-17 21:35:25 +0200

    spostato titolo pacchetti

--- components/Grids/packetGrid.vue
+++ components/Grids/packetGrid.vue
@@ -1,12 +1,4 @@
 <template>
-  <div
-      ref="packagesTitleRef"
-      :class="[$style.packetsTitle, { [$style.titleVisible]: showPackagesTitle }]"
-      class="packages-title"
-  >
-    <h2>🎁 {{currentLang  == 'it' ? 'I nostri pacchetti' : 'Our Packages' }}</h2>
-  </div>
-
   <div :class="$style.courseGrid">
     <Packet
         v-for="packet in packetStore.packets"
@@ -21,48 +13,10 @@
 <script setup lang="ts">
 import Packet from "~/components/Single_Elements/packet.vue";
 import { usePacketStore } from '~/stores/homeRelatedStores/packetStores'
-import {nextTick, onMounted, ref, watch} from "vue";
 import {useLanguage} from "~/composables/useLanguage";
 import {useSupabaseClient} from "#imports";
 
 const packetStore = usePacketStore()
-const { currentLang } = useLanguage()
-const supabase = useSupabaseClient()
-const showPackagesTitle = ref(false)
-const packagesTitleRef = ref<HTMLElement | null>(null)
-
-const setupTitleAnimation = () => {
-  nextTick(() => {
-    const titleElement = document.querySelector('.packages-title')
-
-    if (titleElement) {
-      const observer = new IntersectionObserver(
-          entries => {
-            entries.forEach(entry => {
-              if (entry.isIntersecting) {
-                showPackagesTitle.value = true
-                observer.unobserve(entry.target)
-              }
-            })
-          },
-          {
-            threshold: 0.1,
-            rootMargin: '-100px 0px -100px 0px'
-          }
-      )
-      observer.observe(titleElement)
-    }
-  })
-}
-
-onMounted(() => {
-  setupTitleAnimation()
-
-})
-
-watch(currentLang, () => {
-  packetStore.fetchPackets(currentLang.value, supabase)
-}, { immediate: true })
 </script>
 
 <style module>
@@ -74,39 +28,15 @@ watch(currentLang, () => {
   place-items: center;
 }
 
-.packetsTitle {
-  margin-top: 2rem;
-  font-size: 5rem;
-  font-weight: bold;
-  font-family: 'Rounded Mplus 1c Bold', serif;
-  color: #0769a2;
-  text-align: center;
-  text-shadow: 4px 4px 12px rgba(0, 0, 0, 0.6);
-  opacity: 0;
-  transform: translateX(-200px);
-  transition: opacity 0.6s ease-out, transform 0.6s ease-out;
-}
-.packetsTitle.titleVisible {
-  opacity: 1;
-  transform: translateX(0);
-}
-
-
 @media (max-width: 768px) {
   .courseGrid {
     grid-template-columns: repeat(auto-fit, minmax(8rem, 1fr));
-  }
-  .packetsTitle {
-    font-size: 2.5rem;
   }
 }
 
 @media (min-width: 2560px) {
   .courseGrid {
     grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
-  }
-  .packetsTitle {
-    font-size: 8rem;
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -30,11 +30,19 @@
     <p>Loading...</p>
   </div>
 
+  <div
+      ref="packagesTitleRef"
+      :class="[$style.packetsTitle, { [$style.titleVisible]: showPackagesTitle }]"
+      class="packages-title"
+  >
+    <h2>🎁 {{currentLang  == 'it' ? 'I nostri pacchetti' : 'Our Packages' }}</h2>
+  </div>
+
   <PacketGrid/>
 </template>
 
 <script setup lang="ts">
-import { onMounted, watch} from 'vue'
+import {nextTick, onMounted, ref, watch} from 'vue'
 import { useSupabaseClient } from '#imports'
 import { useLanguage } from '~/composables/useLanguage'
 import Presentation from '~/components/Single_Elements/presentation.vue'
@@ -43,7 +51,6 @@ import { useHomePresentationStore } from '~/stores/homeRelatedStores/homePresent
 import PacketGrid from "~/components/Grids/packetGrid.vue";
 import { useSlideStore } from '~/stores/homeRelatedStores/slideshow'
 
-const supabase = useSupabaseClient()
 const { currentLang } = useLanguage()
 const yogaClass = "yoga"
 const homeStore = useHomePresentationStore()
@@ -61,9 +68,71 @@ watch(currentLang, (lang) => {
   slideStore.fetchImages(currentLang.value)
 }, { immediate: true })
 
+const showPackagesTitle = ref(false)
 
+const setupTitleAnimation = () => {
+  nextTick(() => {
+    const titleElement = document.querySelector('.packages-title')
+
+    if (titleElement) {
+      const observer = new IntersectionObserver(
+          entries => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                showPackagesTitle.value = true
+                observer.unobserve(entry.target)
+              }
+            })
+          },
+          {
+            threshold: 0.1,
+            rootMargin: '-100px 0px -100px 0px'
+          }
+      )
+      observer.observe(titleElement)
+    }
+  })
+}
+
+onMounted(() => {
+  setupTitleAnimation()
+})
 </script>
 
 <style module>
+.packetsTitle {
+  margin-top: 2rem;
+  font-size: 5rem;
+  font-weight: bold;
+  font-family: 'Rounded Mplus 1c Bold', serif;
+  color: #0769a2;
+  text-align: center;
+  text-shadow: 4px 4px 12px rgba(0, 0, 0, 0.6);
+  opacity: 0;
+  transform: translateX(-200px);
+  transition: opacity 0.6s ease-out, transform 0.6s ease-out;
+}
+.packetsTitle.titleVisible {
+  opacity: 1;
+  transform: translateX(0);
+}
 
+
+@media (max-width: 768px) {
+  .courseGrid {
+    grid-template-columns: repeat(auto-fit, minmax(8rem, 1fr));
+  }
+  .packetsTitle {
+    font-size: 2.5rem;
+  }
+}
+
+@media (min-width: 2560px) {
+  .courseGrid {
+    grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+  }
+  .packetsTitle {
+    font-size: 8rem;
+  }
+}
 </style>
